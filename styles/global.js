854f6d1590ec052d16a38c484b0d6d06e5efe5fe
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-06 15:09:29 +0100

    imported StyleSheet component
    included StyleSheet to make global styles work

---
+++ styles/global.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EBEAED',
+        fontFamily: 'sora-regular',
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontFamily: 'sora-bold',
+      },
+
+})
